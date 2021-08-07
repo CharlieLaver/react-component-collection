87c3c8762716d64463b9270608ef526488005d36
Charlie Laver <charlielaver7@gmail.com>
2021-08-07 13:56:59 +0100

    unity portal script

---
+++ C#/Unity/Physics/portal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
